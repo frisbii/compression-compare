bcda78ae8d488aabdb2b727e207eff7b84cdc263
Madi Gudin <mgudin27@amherst.edu>
2026-06-25 16:25:26 -0400

    llvm data and runs sh boilerplate

--- runs.sh
+++ runs.sh
@@ -1,1 +1,15 @@
-cat images/ollama_qwen25_coder_32b.page-images | bin/zlib > data/zlib_ollama-local.csv
+#trace=~sfkaplan/traces/2026-06-22-build-llvm.page-images.xz
+#test=llvm
+
+trace=~sfkaplan/traces/ollama_qwen25_coder_32b.page-images.xz
+test=ollama
+
+
+binaries=(lz4 WKdm zlib)
+outdir=data
+
+for bin in "${binaries[@]}"; do
+    out="${outdir}/${test}_${bin}.csv"
+    printf 'running %s -> %s\n' "$bin" "$out"
+    unxz -c "$trace" | "./bin/$bin" > "$out"
+done
