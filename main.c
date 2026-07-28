0169b9ef3279c79c2d0a6e284f2c0b570b8ecc66
Madi Gudin <mgudin27@amherst.edu>
2026-07-28 15:30:01 -0400

    cleanup + add comments

--- main.c
+++ main.c
@@ -26,6 +26,7 @@
 
 struct Record {
     int page_number;
+    int iteration_number;
     int uncompressed_size;
     int compressed_size;
     time_t compression_time;
@@ -36,6 +37,9 @@ struct Record record;
 
 
 time_t calculate_duration(struct timespec start, struct timespec stop) {
+    /**
+     * Given a timespec start and stop, return the time elapsed in ns
+     */
     uint64_t start_ns = (start.tv_sec * 1e9) + start.tv_nsec;
     uint64_t stop_ns  = (stop.tv_sec  * 1e9) + stop.tv_nsec;
     return stop_ns - start_ns;
@@ -43,6 +47,9 @@ time_t calculate_duration(struct timespec start, struct timespec stop) {
 
 
 void time_compression(word* src, word* dst, size_t buffer_size) {
+    /**
+     * Compress a src buffer of size buffer_size into the dst buffer
+     */
     struct timespec start_time, stop_time;
     clock_gettime(TEST_CLOCK, &start_time);
     size_t compressed_size = compressor.compress(src, dst, buffer_size);
@@ -50,10 +57,6 @@ void time_compression(word* src, word* dst, size_t buffer_size) {
 
     time_t duration = calculate_duration(start_time, stop_time);
 
-    /* printf("Compressed %dB to %dB in %ldns\n", 
-        BYTES_PER_PAGE,
-        compressed_size,
-        ); */
     record.uncompressed_size = BYTES_PER_PAGE;
     record.compressed_size = compressed_size;
     record.compression_time = duration;
@@ -61,13 +64,16 @@ void time_compression(word* src, word* dst, size_t buffer_size) {
 
 
 void time_decompression(word* src, word* dst, size_t buffer_size) {
+    /**
+     * Decompress the content from the dst buffer into the src buffer,
+     * where the we expect the recovered data to be of size buffer_size
+     */
     struct timespec start_time, stop_time;
     clock_gettime(TEST_CLOCK, &start_time);
     compressor.decompress(dst, src, buffer_size);
     clock_gettime(TEST_CLOCK, &stop_time);
 
     time_t duration = calculate_duration(start_time, stop_time);
-    /* printf("Decompressed in %ldns\n", duration); */
     record.decompression_time = duration;
 }
 
@@ -81,6 +87,7 @@ int main(int argc, char *argv[]) {
 
     int pages = 0;
 
+    // create working buffers
     size_t buffer_size = BYTES_PER_PAGE * 2;
     word* src = (word*) malloc(buffer_size);
     word* dst = (word*) malloc(buffer_size);
@@ -90,17 +97,12 @@ int main(int argc, char *argv[]) {
         exit(-1);
     }
 
-    /* char* image_path = "./ollama_qwen25_coder_32b.page-images";
-    FILE* in_stream = fopen(image_path, "r");
-    if (in_stream == NULL) {
-        perror("ERROR: could not open image");
-    } */
     FILE* in_stream = stdin;
-    //printf("page_number,uncompressed_size,compressed_size,compression_time,decompression_time\n");
 
     size_t pages_read;
     int page_counter = 0;
     int memcmp_res;
+    // read in page images from stdin
     while (!feof(in_stream)) {
         page_counter++;
         if (pages > 0 && page_counter >= pages) {
@@ -108,10 +110,12 @@ int main(int argc, char *argv[]) {
         }
         record.page_number = page_counter;
 
+        // clear working buffers
         memset((void*) src, -1, buffer_size);
         memset((void*) dst, -1, buffer_size);
         memset((void*) copy, -1, buffer_size);
 
+        // read the page in
         pages_read = fread(src, BYTES_PER_PAGE, 1, in_stream);
         if (pages_read != 1) {
             if (feof(in_stream)) {
@@ -125,23 +129,27 @@ int main(int argc, char *argv[]) {
         memcpy((void*) copy, (void*) src, buffer_size);
         assert(memcmp((void*) src, (void*) copy, buffer_size) == 0);
 
+        // actual work
         time_compression(src, dst, buffer_size);
 
         memset((void*) src, -1, buffer_size);
 
         time_decompression(src, dst, buffer_size);
 
-        // checks
+        // verify that we recovered what we put in
         memcmp_res = memcmp((void*) src, (void*) copy, BYTES_PER_PAGE);
         if (memcmp_res != 0) {
             printf("\tHELP! %d bytes off; %d pages in\n", memcmp_res, page_counter);
             break;
         }
 
-        // page_number,uncompressed_size,compressed_size,compression_time,decompression_time
         printf("%d,%d,%d,%ld,%ld\n",
-            record.page_number, record.uncompressed_size, record.compressed_size,
-            record.compression_time, record.decompression_time);
+            record.page_number, 
+            record.uncompressed_size, 
+            record.compressed_size,
+            record.compression_time, 
+            record.decompression_time
+        );
 
     }
     
